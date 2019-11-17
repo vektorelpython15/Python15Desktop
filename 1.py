42d67c90f9b2b37a0bba6958c9d0d0b7f8b3aea9
İbrahim Ediz <Kurs>
2019-11-17 15:13:03 +0200

    17112019_2

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget,QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton,QLineEdit
 from PyQt5.QtGui import QIcon
 class App(QWidget):
     def __init__(self):
@@ -13,13 +13,30 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.button = QPushButton("Tıkla",self)
+
+        self.button = QPushButton("BT 1",self)
         self.button.clicked.connect(self.tiklandi)
         self.button.move(10,20)
+        
+        self.button1 = QPushButton("BT 2",self)
+        self.button1.clicked.connect(self.tiklandi)
+        self.button1.move(10,40)
+
+        self.button2 = QPushButton("BT 3",self)
+        self.button2.clicked.connect(self.tiklandi)
+        self.button2.move(10,60)
+
+        self.txtBox = QLineEdit(self)
+        self.txtBox.move(10,80)
+
         self.show()
 
     def tiklandi(self):
-        print("Tıklandı")
+        print("Tıklandı ",self.sender().text())
+    
+    def textControl(self):
+        girilen = self.txtBox.text()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
