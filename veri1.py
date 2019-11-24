140f4225189085cface54b6471c61dada0fdffa3
İbrahim Ediz <ibrahim.ediz@gazi.edu.tr>
2019-11-24 13:37:05 +0200

    pull için deneme

--- veri1.py
+++ veri1.py
@@ -1,4 +1,21 @@
 from PyQt5.QtWidgets import QApplication,QMainWindow,QLineEdit
 from PyQt5 import uic
+#veri tabanı nesnesinin çağırılması
 
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.win = uic.loadUi("veri1.ui")
+        #### Bu kısım düğmeye fonksiyonun atanacağı kısım
+        self.win.show()
 
+    def kaydet(self):
+        # veri tabanı fonksiyonu üzerinden txt içine yazılmış 
+        # verinin kaydedilmesi
+        # olumlu yanıtın ekrana yansıtılması
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
